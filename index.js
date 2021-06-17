0ce92dc98c714e700cc66cf30fb954e75638d968
vsumit698 <50446660+vsumit698@users.noreply.github.com>
2021-06-17 12:26:59 +0530

    updated code

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ if ( process.env.NODE_ENV == "production"){
 
     app.use(express.static("frontend/build"));
 
-    // const path = require("path");
+    const path = require("path");
 
-    // app.get("*", (req, res) => {
+    app.get("*", (req, res) => {
 
-    //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 
-    // })
+    });
 
 }
 
